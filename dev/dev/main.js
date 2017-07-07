fe90086279870036d4f539eeefbcbb612586c5e5
Bijiabo <bijiabo@gmail.com>
2017-07-08 01:08:43 +0800

    What: * 添加天气获取 API 调用 Why：
    How:

--- dev/dev/main.js
+++ dev/dev/main.js
@@ -5,18 +5,36 @@ setInterval(function () {
     window.currentTime = new Date();
     document.getElementById('clock').textContent = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()} `;
     
-    BridgeAPI.sendDataToReactNative(currentTime);
+    // BridgeAPI.sendDataToReactNative(currentTime);
+    // getWeatherData();
 }, 1000);
 
-// window.addEventListener('BridgeAPIReady', function () {
-//     $('.log').append('BridgeAPIReady\n');
-// });
+var getWeatherData = function () {
+    BridgeAPI.sendDataToReactNative({
+        action: 'weather.now'
+    });
+};
+
+var cache = {};
 
 $(window).on('BridgeAPIReady', function () {
     $('.log').append('BridgeAPIReady\n');
     
+    getWeatherData();
+    
     BridgeAPI.addChangeWatcher(function (data) {
         console.log(data);
-        $('.log').append(JSON.stringify(data) + '\n');
+        switch (data.action) {
+            case 'weather.now':
+                if (JSON.stringify(cache.weatherNow) != JSON.stringify(data.data)) {
+                    $('.log').append(JSON.stringify(data) + '\n');
+                    cache.weatherNow = data.data;
+                }
+                
+                break;
+            default:
+                break;
+        }
+        // $('.log').append(JSON.stringify(data) + '\n');
     });
 });
